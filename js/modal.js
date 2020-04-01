434b626ed9dfa908ae9df802036abc733849ac6e
pieter-dirix <pieter.dirix@student.ehb.be>
2020-04-01 13:44:23 +0200

    cleanup

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,13 @@
 let artists_content = document.getElementsByClassName("artists_content")[0];
 let contact = document.getElementsByClassName("contact")[0];
+let artists_content_mobile = document.getElementsByClassName("artists_content_mobile")[0];
 let logo = document.getElementById("header_logo");
 let footer = document.getElementsByTagName("footer")[0];
 
 let mvnsi = document.getElementById("modal_mvnsi");
 let farflow = document.getElementById("modal_farflow");
 let c1p = document.getElementById("modal_c1p");
-
+let boshra = document.getElementById("modal_boshra");
 
 let tevin = document.getElementById("modal_tevin");
 let jojo = document.getElementById("modal_jojo");
@@ -25,30 +26,91 @@ let modals = document.getElementsByClassName("modal");
 
 let span = document.getElementsByClassName("close")[0];
 
-let links_buumi = document.getElementById("links_buumi");
+let isMobile = false;
+let modalOpen = false;
+
+const mobileTresh = 500;
+
+window.onload = function () {
+    if (window.innerWidth <= mobileTresh) {
+        isMobile = true;
+        artists_content.style.display = "none"
+        artists_content_mobile.style.display = "block"
+    } else {
+        isMobile = false;
+        artists_content.style.display = "block"
+        artists_content_mobile.style.display = "none"
+    }
+}
 
 function blurBackground() {
-    artists_content.style.display = "none";
-    artists_content.style.filter = "blur(4px)";
-    contact.style.display= "none";
-    contact.style.filter = "blur(2px)";
-    footer.style.filter = "blur(2px)";
-    logo.style.filter = "blur(2px)";
+    modalOpen = true;
+    if (isMobile) {
+
+        body.style.overflow = "hidden";
+
+        artists_content_mobile.style.filter = "blur(4px)";
+        artists_content_mobile.style.opacity = "0.3"
+        footer.style.filter = "blur(2px)";
+        logo.style.filter = "blur(2px)";
+        contact.style.filter = "blur(2px)";
+        // artists_content_mobile.style.display = "none"
+        // footer.style.display = "none";
+        // logo.style.display = "none";
+    } else {
+        artists_content.style.display = "none";
+        artists_content.style.filter = "blur(4px)";
+        footer.style.filter = "blur(2px)";
+        logo.style.filter = "blur(2px)";
+        contact.style.display = "none";
+
+    }
 
 }
+
+
+
 
 function unBlurBackground() {
-    artists_content.style.display = "block";
-    artists_content.style.filter = "blur(0)";
-    contact.style.filter = "blur(0)";
-    contact.style.display= "inline-block";
-    footer.style.filter = "blur(0)";
-    logo.style.filter = "blur(0)";
-
+    modalOpen = false;
+    if (isMobile) {
+        body.style.overflow = "visible";
+        artists_content_mobile.style.display = "block"
+        artists_content_mobile.style.filter = "blur(0px)";
+        artists_content_mobile.style.opacity = "1"
+        footer.style.display = "flex";
+        logo.style.display = "block";
+        footer.style.filter = "blur(0)";
+        logo.style.filter = "blur(0)";
+        contact.style.filter = "blur(0)";
+    } else {
+        artists_content.style.display = "block";
+        artists_content.style.filter = "blur(0px)";
+        footer.style.filter = "blur(0)";
+        logo.style.filter = "blur(0)";
+        contact.style.display = "inline-block";
+    }
 }
 
+
+// window.onscroll = function() {
+    
+    
+//     if(modalOpen) {
+//         console.log(window.pageYOffset)
+//         window.scrollTo(window.pageXOffset, window.pageYOffset);
+//     }
+
+// } 
+
 window.onclick = function (event) {
-    console.log(event.target);
+
+    if (window.innerWidth <= mobileTresh) {
+        isMobile = true;
+    } else {
+        isMobile = false;
+    }
+
     for (let index = 0; index < modals.length; index++) {
         const element = modals[index];
         if (event.target == element) {
@@ -57,20 +119,36 @@ window.onclick = function (event) {
             close_modal(id);
         }
     }
-    if(event.target == contact_container) {
+    if (event.target == contact_container) {
         closeContact();
     }
+
+}
+
+window.onresize = function (event) {
+    if (window.innerWidth <= mobileTresh) {
+        isMobile = true;
+        artists_content.style.display = "none"
+        artists_content_mobile.style.display = "block"
+    } else {
+        isMobile = false;
+        artists_content.style.display = "block"
+        artists_content_mobile.style.display = "none"
+    }
+};
+
+function closeContact() {
+    contact_container.style.display = "none";
+    unBlurBackground();
     
 }
 
-function closeContact() {
-    unBlurBackground();
-    contact_container.style.display = "none";
-}
-
 function openContact() {
-    blurBackground();
+    console.log(window.pageYOffset);
+    window.scrollTo(window.pageXOffset, window.pageYOffset);
     contact_container.style.display = "block";
+    blurBackground();
+    
 
 }
 
@@ -112,7 +190,9 @@ function close_modal(clicked_id) {
         case "close_buumi":
             buumi.style.display = "none";
             break;
-
+        case "close_boshra":
+            boshra.style.display = "none";
+            break;
         default:
             break;
     }
@@ -160,26 +240,32 @@ function open_modal(clicked_id) {
             buumi.style.display = "block";
             break;
 
+        case "boshra":
+            boshra.style.display = "block";
+            break;
         default:
             break;
     }
 
-    
-    
+
+
     for (let index = 0; index < modals.length; index++) {
         const element = modals[index];
-        
+
         var style = window.getComputedStyle(element);
-        if(!(style.display === 'none')){
+        if (!(style.display === 'none')) {
             let imgContainer = element.getElementsByClassName("modal_img_container")[0];
             let links = element.getElementsByClassName("modal_bottom")[0];
             
-            links.style.height = imgContainer.offsetWidth + 'px';
+            if(!isMobile) {
+                links.style.height = imgContainer.offsetWidth + 'px';
+            }
+            
 
             console.log(element.childNodes[3].childNodes);
         }
-        
+
     }
-    
+
 
 }
